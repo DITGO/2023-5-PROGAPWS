78101eb185313a3fadda8f606fd5c97ee2fc4128
Jose Henrique Guedes <henriquealvesguedes1912@gmail.com>
2023-06-06 08:50:07 -0300

    commit token

--- src/Utils/functionsToken.ts
+++ src/Utils/functionsToken.ts
@@ -6,6 +6,30 @@ export async function verifyToken(
   response: Response,
   next: NextFunction,
 ) {
-  const token = request.headers['token_sso'];
-  return response.status(200).json(request.headers['token_sso']);
+  const token = request.headers['token'];
+
+  if (!token) {
+    return response
+      .status(401)
+      .json({ auth: false, message: 'O token não foi fornecido!' });
+  }
+
+  try {
+    const validationUrl = `https://ssows-h.ssp.go.gov.br/validate?token=${token}`;
+
+    const apiResponse = await axios.get(validationUrl);
+    if (!apiResponse.data.token || apiResponse.data.token === '') {
+      return response.status(401).json({
+        auth: false,
+        message: 'Token inválido!',
+      });
+    }
+
+    next();
+  } catch (error) {
+    return response.status(500).json({
+      auth: false,
+      message: 'Erro ao autenticar o token!',
+    });
+  }
 }
