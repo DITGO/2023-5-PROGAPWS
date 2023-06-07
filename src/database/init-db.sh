1d496160cd8df772af14e31c2d0a36d94ba3f012
Jose Henrique Guedes <henriquealvesguedes1912@gmail.com>
2023-06-07 16:46:37 -0300

    script postgres

--- src/database/init-db.sh
+++ src/database/init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "api_servidores" --dbname "gap_project" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
 	CREATE USER api_servidores;
   ALTER USER api_servidores PASSWORD 'api_password';
 	CREATE DATABASE gap_project;
