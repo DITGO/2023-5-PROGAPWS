956624f7c9921bff00b76fd2065cfdb311f27f55
Jose Henrique Guedes <henriquealvesguedes1912@gmail.com>
2023-06-12 10:28:57 -0300

    teste

--- src/database/init-db.sh
+++ src/database/init-db.sh
@@ -4,6 +4,6 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
   CREATE USER api_servidores;
   ALTER USER api_servidores PASSWORD 'api_password';
-  CREATE DATABASE gap_project;
+  SELECT 'CREATE DATABASE gap_project' WHERE NOT EXISTS (SELECT FROM pg_database datname = 'gap_project')\gexec
 	GRANT ALL PRIVILEGES ON DATABASE gap_project TO api_servidores;
 EOSQL
