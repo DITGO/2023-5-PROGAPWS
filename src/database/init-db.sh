497815a1d09516d64291dcb7483a7f1611d1bdf3
Jose Henrique Guedes <henriquealvesguedes1912@gmail.com>
2023-06-12 14:25:10 -0300

    database

--- src/database/init-db.sh
+++ src/database/init-db.sh
@@ -1,9 +1,27 @@
+# #!/bin/bash
+# set -e
+
+# psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
+# 	CREATE USER api_servidores;
+#     ALTER USER api_servidores PASSWORD 'api_password';
+# 	CREATE DATABASE gap_project;
+# 	GRANT ALL PRIVILEGES ON DATABASE gap_project TO api_servidores;
+# EOSQL
+
+
 #!/bin/bash
 set -e
-
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
-	CREATE USER api_servidores;
+    CREATE USER IF NOT EXISTS api_servidores;
     ALTER USER api_servidores PASSWORD 'api_password';
-	CREATE DATABASE gap_project;
-	GRANT ALL PRIVILEGES ON DATABASE gap_project TO api_servidores;
+
+    -- Verifica se o banco de dados já existe
+    \c gap_project
+    SELECT 1 FROM pg_database WHERE datname = 'gap_project' LIMIT 1;
+
+    -- Se o banco de dados não existir, cria-o e concede privilégios
+    \ifcode == 0
+    CREATE DATABASE gap_project;
+    GRANT ALL PRIVILEGES ON DATABASE gap_project TO api_servidores;
+    \endif
 EOSQL
