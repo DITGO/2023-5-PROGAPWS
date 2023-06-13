d76d801d8bc5f3774d0a003c70cdc2a79055f0b5
Jose Henrique Guedes <henriquealvesguedes1912@gmail.com>
2023-06-13 09:34:35 -0300

    database

--- src/database/init-db.sh
+++ src/database/init-db.sh
@@ -1,23 +1,10 @@
-# #!/bin/bash
-# set -e
-
-# psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
-# CREATE USER api_servidores;
-# ALTER USER api_servidores PASSWORD 'api_password';
-# SELECT 'CREATE DATABASE gap_project' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'gap_project')\gexec
-# GRANT ALL PRIVILEGES ON DATABASE gap_project TO api_servidores;
-# EOSQL
-
 #!/bin/bash
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
-    CREATE USER api_servidores;
-    ALTER USER api_servidores PASSWORD 'api_password';
+CREATE USER api_servidores;
+ALTER USER api_servidores PASSWORD 'api_password';
+SELECT 'CREATE DATABASE gap_project' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'gap_project')\gexec
+GRANT ALL PRIVILEGES ON DATABASE gap_project TO api_servidores;
 EOSQL
 
-database_exists=$(psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='gap_project'")
-
-if [ "$database_exists" != "1" ]; then
-    createdb -U postgres -O api_servidores gap_project
-fi
