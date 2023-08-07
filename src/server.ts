28687aeeb3d3d3c9dca02c53f1ca980bc70b8c7e
Jose Henrique Guedes <henriquealvesguedes1912@gmail.com>
2023-08-07 15:09:12 -0300

    env

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { APPDataSource } from './database/data-source';
 APPDataSource.initialize().then(() => {
   app.listen(3011, () =>
     console.log(
-      'Server is running! 🏆 Open http://localhost:3011 to see results',
+      'Server is running!  🏆 Open http://localhost:3011 to see results',
     ),
   );
 });
